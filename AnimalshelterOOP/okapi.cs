0e9b82b41b1eb2958ba7aad57442107d8ff4251e
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-04-28 14:33:38 +0200

    initialized okapi class

---
+++ AnimalshelterOOP/okapi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalshelterOOP
+{
+    internal class okapi:Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("chuff");
+            Name = "okapi";
+        }
+    }
+}
