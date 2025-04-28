0dd689cdd941cf6d00dd8cf26da2cb9ddc1954a3
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-04-28 11:09:28 +0200

    cretaed whaleshark class

---
+++ AnimalshelterOOP/whaleshark.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalshelterOOP
+{
+    internal class whaleshark:Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("gluckgluck");
+            Name = "whaleshark";
+        }
+    }
+}
