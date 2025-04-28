ddd0c045d24cead98e31b71bec98697f90338eca
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-04-28 14:27:43 +0200

    initialize dog class

---
+++ AnimalshelterOOP/dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalshelterOOP
+{
+    internal class dog:Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("wuff");
+            Name = "dog";
+        }
+    }
+}
