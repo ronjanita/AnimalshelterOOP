359cb318fb01271497ee0db7204706812c9d3497
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-05-09 09:30:55 +0200

    added tigershark

---
+++ AnimalshelterOOP/tigershark.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalshelterOOP
+{
+    internal class Tigershark : Animal
+    {
+        public Tigershark(string name, int age, string species) : base(name, age, species) { }
+        public override void MakeSound()
+        {
+            Console.WriteLine("pluckpluck");
+        }
+        public override string ToString()
+        {
+            return $"{Species},{Name},{Age}years old.";
+
+        }
+    }
+}
