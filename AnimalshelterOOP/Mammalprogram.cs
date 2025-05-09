aad9bb6928731d8f49c5927186b22e59ed5b0461
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-05-09 11:30:13 +0200

    added mammalprogram class and its ocntent

---
+++ AnimalshelterOOP/Mammalprogram.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalshelterOOP
+{
+    internal class Mammalprogram
+    {
+        static public ShelterMammal mammalShelter = new ShelterMammal();
+        public static void CreateExistingMammal()
+        {
+            Dog newDog = new("leo", 1, "dog");
+            Console.WriteLine(newDog);
+            newDog.MakeSound();
+            Lion newLion = new("rocky", 14, "lion");
+            Console.WriteLine(newLion);
+            newLion.MakeSound();
+            Okapi newOkapi = new("rudolf mc donald", 8, "okapi");
+            Console.WriteLine(newOkapi);
+            newOkapi.MakeSound();
+            Cat newCat = new("sissi", 12, "cat");
+            Console.WriteLine(newCat);
+            newCat.MakeSound();
+
+            mammalShelter.AddAnimal(newDog);
+            mammalShelter.AddAnimal(newLion);
+            mammalShelter.AddAnimal(newOkapi);
+            mammalShelter.AddAnimal(newCat);
+        }
+
+        static void EnterNewMammal()
+        {
+            Console.WriteLine("Please enter the species of the new animal:");
+            string species = Console.ReadLine().ToLower();
+            Console.WriteLine("Please enter the name of the animal:");
+            string nameNewAnimal = Console.ReadLine().ToLower();
+            Console.WriteLine("Please enter the age of the animal:");
+            int ageNewAnimal = int.Parse(Console.ReadLine());   //int parse macht string in int
+            switch (species)
+            {
+                case "dog":
+                    Dog newDogToAdd = new Dog(nameNewAnimal, ageNewAnimal, species);
+                    mammalShelter.AddAnimal(newDogToAdd);
+                    break;
+                case "lion":
+                    Lion newLionToAdd = new Lion(nameNewAnimal, ageNewAnimal, species);
+                    mammalShelter.AddAnimal(newLionToAdd);
+                    break;
+                case "okapi":
+                    Okapi newOkapiToAdd = new Okapi(nameNewAnimal, ageNewAnimal, species);
+                    mammalShelter.AddAnimal(newOkapiToAdd);
+                    break;
+                case "cat":
+                    Cat newCatToAdd = new Cat(nameNewAnimal, ageNewAnimal, species);
+                    mammalShelter.AddAnimal(newCatToAdd);
+                    break;
+                default:
+                    Animal newAnimal = new Animal(nameNewAnimal, ageNewAnimal, species);
+                    mammalShelter.AddAnimal(newAnimal);
+                    break;
+            }
+        }
+        static void AdoptNewMammal()
+        {
+            Console.WriteLine("Please enter the name of the animal you want to adopt:");
+            string adoptedMammalName = Console.ReadLine().ToLower();
+            mammalShelter.AdoptAnimal(adoptedMammalName);
+        }
+
+        public void GreetNewMammal()
+        {
+            Console.WriteLine("enter the name of the species you want to greet:");
+            string mammalGreet = Console.ReadLine().ToLower();
+            mammalShelter.GreetAnimal(mammalGreet);
+        }
+        static void RunGame()
+        {
+            Console.WriteLine("Chose between: \n - add a new animal to the shelter(1) \n - adopt an animal from the shelter(2) \n - show all animals(3) \n - greet animal(4)");
+            string selection = Console.ReadLine().ToLower();
+
+            switch (selection)
+            {
+                case "1":
+                    EnterNewMammal();
+                    break;
+                case "2":
+                    AdoptNewMammal();
+                    break;
+                case "3":
+                    mammalShelter.ShowAllAnimals();
+                    break;
+                case "4":
+                    Mammalprogram mammalprogram = new();
+                    mammalprogram.GreetNewMammal();
+                    break;
+            }
+        }
+    }
+}
