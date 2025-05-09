cba3d03cb3dd1290a417317b37549737c92ebb61
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-05-09 09:35:41 +0200

    added stingray

---
+++ AnimalshelterOOP/stingray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalshelterOOP
+{
+    internal class Stingray : Animal
+    {
+        public Stingray(string name, int age, string species) : base(name, age, species) { }
+        public override void MakeSound()
+        {
+            Console.WriteLine("clickclick");
+        }
+        public override string ToString()
+        {
+            return $"{Species},{Name},{Age}years old.";
+
+        }
+    }
+
+}
