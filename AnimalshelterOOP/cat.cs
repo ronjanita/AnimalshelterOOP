6ba27ad3894a0ca4353dfec0756cde4da4fe36ec
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-05-09 10:30:35 +0200

    added cat class and its content

---
+++ AnimalshelterOOP/cat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalshelterOOP
+{
+    public class Cat : Animal
+    {
+        public Cat(string name, int age, string species) : base(name, age, species) { }
+        public override void MakeSound()
+        {
+            Console.WriteLine("miau");
+        }
+        public override string ToString()
+        {
+            return $"{Species},{Name},{Age}years old.";
+
+        }
+    }
+}
