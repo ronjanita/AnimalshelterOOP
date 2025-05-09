0c25c15c5b278ae9106cc48a4af83293e308ecd5
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-05-09 09:04:48 +0200

    added ShelterFishs class and its content

---
+++ AnimalshelterOOP/ShelterFishs.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalshelterOOP
+{
+    public class ShelterFishs
+    {
+        public List<Animal> fishsInShelter = new();
+
+        public void AddAnimal(Animal fishs)   //fügt ein tier zu liste hinzu
+        {
+            fishsInShelter.Add(fishs);
+        }
+        public void AdoptAnimal(string searchingName) //entfernt ein tier anhand des namens aus der liste
+        {
+            Animal foundFishs = null;
+            foreach (Animal fishsToCheck in fishsInShelter)  // rückwerts durch lsite itieren?
+            {
+                if (fishsToCheck.Name == searchingName)
+                {
+                    foundFishs = fishsToCheck;
+                }
+            }
+            if (foundFishs != null)
+            {
+                fishsInShelter.Remove(foundFishs);
+            }
+
+            //Animal searchedAnimal = animalsInShelter.FirstOrDefault(animalToCheck => animalToCheck.Name == searchingName);
+            //animalsInShelter.Remove(searchedAnimal);
+        }
+        public void ShowAllAnimals()
+        {
+            foreach (Animal animal in fishsInShelter)
+            {
+                Console.WriteLine($"here are all animals currently in the shelter: {animal}");
+            }
+        }
+
+        public void GreetAnimal(string searchingSpecies)
+        {
+            Animal foundFishs = null;
+            foreach (Animal animalToCheck in fishsInShelter)
+            {
+                if (animalToCheck.Species == searchingSpecies)
+                {
+                    foundFishs = animalToCheck;
+                }
+            }
+            if (foundFishs != null)
+            {
+                foundFishs.MakeSound();
+            }
+        }
+    }
+}
