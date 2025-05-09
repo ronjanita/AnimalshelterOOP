86571c697bfe0710fbe99d5515a194c92e72c562
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-05-09 10:22:32 +0200

    added toadfish and its ocntent

---
+++ AnimalshelterOOP/toadfish.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalshelterOOP
+{
+    internal class Toadfish : Animal
+    {
+        public Toadfish(string name, int age, string species) : base(name, age, species) { }
+        public override void MakeSound()
+        {
+            Console.WriteLine("growlgrowl");
+        }
+        public override string ToString()
+        {
+            return $"{Species},{Name},{Age}years old.";
+
+        }
+    }
+}
