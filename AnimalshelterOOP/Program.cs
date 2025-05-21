3f1afb99a0cea095e883b55bd75627d32b94135f
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-05-21 08:03:59 +0200

    finished maingame logic

--- AnimalshelterOOP/Program.cs
+++ AnimalshelterOOP/Program.cs
@@ -15,21 +15,24 @@ namespace AnimalshelterOOP
             bool playAgain = true;
             Console.WriteLine("Welcome to the Animal shelter, choose between the three animalshelters to continue. \n -Mammalshelter (1) \n -Amphibianshelter(2) \n Fishshelter(3)");
             string userChoose = Console.ReadLine();
-            switch (userChoose)
-            {
-                case "1":
-                    Mammalprogram programmammal = new();
-                    programmammal.RunGameMammal();
-                    break;
-                case "2":
-                    Amphibiansprogram programamphibian = new();
-                    programamphibian.RunGameAmphibian();
-                    break;
-
-            }
             while (playAgain)
             {
-                RunGame();
+                switch (userChoose)
+                {
+                    case "1":
+                        Mammalprogram programmammal = new();
+                        programmammal.RunGameMammal();
+                        break;
+                    case "2":
+                        Amphibiansprogram programamphibian = new();
+                        programamphibian.RunGameAmphibian();
+                        break;
+                    case "3":
+                        Fishsprogram programfish = new();
+                        programfish.RunGameFish();
+                        break;
+                }
+                
                 Console.WriteLine("Do you want to end this program? y/n");
                 string input = Console.ReadLine().ToLower();
 
